29448dc0b3ecfcb58d78abfe9ee75c9313010bd4
TurkishLinuxUser <yahyaatabalazade20100@gmail.com>
2024-06-18 03:01:41 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 1. Sistem türünü kontrol etme
 if [ -f /etc/debian_version ]; then
     DISTRO="debian"
 elif [ -f /etc/redhat-release ]; then
@@ -12,7 +11,6 @@ else
     exit 1
 fi
 
-# 2. curl ve wget kontrolü
 if command -v curl &> /dev/null; then
     DOWNLOAD_CMD="curl -L -o"
 elif command -v wget &> /dev/null; then
@@ -29,18 +27,19 @@ else
     DOWNLOAD_CMD="wget -O"
 fi
 
-# 3. Zip dosyasını indirme ve çıkarma
 ZIP_URL="https://github.com/TurkishLinuxUser/Adhan/releases/download/1.0.0/adhan.zip" 
 ZIP_FILE="/tmp/adhan.zip"
 
 $DOWNLOAD_CMD $ZIP_FILE $ZIP_URL
 sudo unzip -o $ZIP_FILE -d ~/.local/share/
 
-# 4. /usr/bin içerisine adhan dosyası oluşturma
+chmod +x languages.json
+chmod +x settings.json
+chmod +x Adhan
+
 echo "~/.local/share/adhan/Adhan" | sudo tee /usr/bin/adhan > /dev/null
 sudo chmod +x /usr/bin/adhan
 
-# 5. /usr/share/applications içerisine kısayol oluşturma
 cat << EOF | sudo tee /usr/share/applications/adhan.desktop > /dev/null
 [Desktop Entry]
 Name=Adhan
